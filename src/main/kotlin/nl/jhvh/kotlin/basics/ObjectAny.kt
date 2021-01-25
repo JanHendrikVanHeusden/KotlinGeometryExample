d83983efab69598cff468bc820d311b60fbc36e6
jvanheusden <janhvh@gmail.com>
2021-01-25 13:38:21 +0100

    Made ObjectAny demo working

--- src/main/kotlin/nl/jhvh/kotlin/basics/ObjectAny.kt
+++ src/main/kotlin/nl/jhvh/kotlin/basics/ObjectAny.kt
@@ -9,21 +9,29 @@ class ObjectAny {
 
 @Suppress("USELESS_IS_CHECK")
 fun main() {
+    println()
+    println("Do not use Object in Kotlin!! Here for this (somewhat weird) demo only!!")
+    println()
     val ref = ObjectAny()
+
     val anyIsAny = (ref.any is Any)
     val anyIsObject = (ref.any is Object)
     val objectIsAny = (ref.anObject is Any)
     val objectIsObject = (ref.anObject is Object)
 
-    println("Any is Any: $anyIsAny")
-    println("Any is Object: $anyIsObject")
-    println("Object is Any: $objectIsAny")
-    println("Object is Object: $objectIsObject")
+    println("Any is Any: $anyIsAny")             // true
+    println("Any is Object: $anyIsObject")       // true
+    println("Object is Any: $objectIsAny")       // true
+    println("Object is Object: $objectIsObject") // true
+    println()
 
+    // Object.notify() - compiles
     try {
         ref.anObject.notifyAll()
     } catch (e: IllegalMonitorStateException) {
-        println("Just showing that it compiles, but don't do this!")
+        println("Just showing that Object.notifyAll() compiles, but don't do this!")
     }
-    // ref.any.notify() // Does not compile
+
+    // Any.notify() - Does not compile
+    println("Any.notifyAll() does NOT compile...!")
 }
