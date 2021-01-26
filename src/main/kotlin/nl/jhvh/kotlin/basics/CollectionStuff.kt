a72adffc537ff5f62c0105957381efe0ed3303af
jvanheusden <janhvh@gmail.com>
2021-01-26 21:56:13 +0100

    Collection demo

--- src/main/kotlin/nl/jhvh/kotlin/basics/CollectionStuff.kt
+++ src/main/kotlin/nl/jhvh/kotlin/basics/CollectionStuff.kt
@@ -1,9 +1,6 @@
 package nl.jhvh.kotlin.basics
 
 import kotlin.random.Random
-import kotlin.time.ExperimentalTime
-
-fun drop3(list: List<String>) = list.drop(3)
 
 val isNumericString = { str: String -> str.matches(Regex("""-?\d+(\.\d+)?(([Ee])-?\d+)?""")) }
 fun numericStrings(list: List<String>) = list.filter { isNumericString(it) }
@@ -11,10 +8,9 @@ fun numericStrings(list: List<String>) = list.filter { isNumericString(it) }
 val myList = listOf("1234", "5678", "test", Random.nextDouble().toString(), (-1.23e-50).toString(), "check1")
 val myLongList =  (1..100_000).toList()
 
-@ExperimentalTime
 fun main() {
 
-    println(drop3(myList))
+    println(myList.drop(3))
     println(numericStrings(myList))
 
     println("Unfiltered size: ${myLongList.size}")
