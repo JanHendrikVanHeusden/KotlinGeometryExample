726653274489526bb7e8b379774741515a798410
jvanheusden <janhvh@gmail.com>
2021-01-26 12:27:52 +0100

    Added demo on Gson safe data class

--- src/main/kotlin/nl/jhvh/kotlin/geometry/model/twodimensional/GsonSafeParallelogram.kt
+++ src/main/kotlin/nl/jhvh/kotlin/geometry/model/twodimensional/GsonSafeParallelogram.kt
@@ -38,7 +38,7 @@ data class GsonSafeParallelogram constructor(val s1: Double, val s2: Double, val
 
     private fun validateInput() {
         require(s1 >= 0.0 && s2 >= 0.0) { "Lengths of both sides must be positive, but sides s1 , s2 are $s1 , $s2" }
-        require(angleDegrees in minAngleDegrees..maxAngleDegrees) { "The angle of a parallelogram must be in range $minAngleDegrees and $maxAngleDegrees, but is $angleDegrees" }
+        require(angleDegrees in 0.0..90.0) { "The angle of a parallelogram must be in range 0.0 and 90.0, but is $angleDegrees" }
     }
 
     override fun compareTo(other: TwoDimensional): Int =
