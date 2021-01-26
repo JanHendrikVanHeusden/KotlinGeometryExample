e28f35b1da85735b79b94a21c5e59cd1810edadf
jvanheusden <janhvh@gmail.com>
2021-01-26 15:02:58 +0100

    Demo Kotlin parameter enhancements

--- src/main/kotlin/nl/jhvh/kotlin/basics/overloading/ParameterDefaultsAndNamedNotation.kt
+++ src/main/kotlin/nl/jhvh/kotlin/basics/overloading/ParameterDefaultsAndNamedNotation.kt
@@ -1,21 +1,20 @@
 package nl.jhvh.kotlin.basics.overloading
 
 import java.nio.charset.Charset
-import java.nio.charset.StandardCharsets
 import java.nio.charset.StandardCharsets.ISO_8859_1
 import java.nio.charset.StandardCharsets.US_ASCII
+import java.nio.charset.StandardCharsets.UTF_8
 import java.util.Locale
 
-@Suppress("unused", "UNUSED_PARAMETER")
-        /** In Java this requires 4 overloads! */
+val defaultLocale: Locale = Locale.getDefault()
+
+/** In Java this requires 4 overloads! */
 fun handleString(
     string: String,
-    locale: Locale = Locale.getDefault(),
-    charset: Charset = StandardCharsets.UTF_8
+    locale: Locale? = null,
+    charset: Charset = UTF_8
 ) {
-    // just do something. locale and charset are ignored, this demo is just
-    // to show parameter default values and positional notation
-    println(string)
+    println("${locale ?: "\t"} \t\t${string.toByteArray(charset).size} bytes \t\t $string")
 }
 
 fun main() {
@@ -23,10 +22,10 @@ fun main() {
 
     // You can use named or positional notation, matter of taste (or coding guidelines)
     handleString("01-31-2021", Locale.US)
-    handleString("01-31-2021", locale = Locale.US)
+    handleString("31-01-2021", locale = defaultLocale)
 
     handleString("\u20ac", charset = ISO_8859_1)
-    handleString(string ="January 31, 2021", Locale.US, US_ASCII)
+    handleString(string = "January 31, 2021", Locale.US, US_ASCII)
 
     // You can even choose a different order of params - not very useful though
     handleString(charset = US_ASCII, locale = Locale.US, string = "January 31, 2021")
