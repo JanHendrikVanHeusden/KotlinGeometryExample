beca630c9788108bfe46e9b7ad5bee400a153527
jvanheusden <janhvh@gmail.com>
2021-01-26 12:34:28 +0100

    Demo nullable / not nullable

--- src/main/kotlin/nl/jhvh/kotlin/basics/Nullability.kt
+++ src/main/kotlin/nl/jhvh/kotlin/basics/Nullability.kt
@@ -3,12 +3,12 @@ package nl.jhvh.kotlin.basics
 fun main() {
     var nullableString: String? = "some text"
     // var notNullable: String = nullableString  // Does not compile -> Required: String   Found: String?
-    var notNullable: String = nullableString?:"" // elvis operator
+    var notNullable: String = nullableString ?: "" // elvis operator
 
     nullableString = null // OK
     println(nullableString)          // null
     println(notNullable.isEmpty())   // false
 
-    notNullable = nullableString?:""
+    notNullable = nullableString ?: ""
     println(notNullable.isEmpty())   // true
 }
