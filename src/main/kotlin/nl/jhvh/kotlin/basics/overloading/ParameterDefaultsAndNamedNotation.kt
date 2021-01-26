116f60c83e554e1d474729dd379393218ea4de6e
jvanheusden <janhvh@gmail.com>
2021-01-26 15:13:16 +0100

    Demo Kotlin parameter enhancements

--- src/main/kotlin/nl/jhvh/kotlin/basics/overloading/ParameterDefaultsAndNamedNotation.kt
+++ src/main/kotlin/nl/jhvh/kotlin/basics/overloading/ParameterDefaultsAndNamedNotation.kt
@@ -14,6 +14,7 @@ fun handleString(
     locale: Locale? = null,
     charset: Charset = UTF_8
 ) {
+    // Don't use toByteArray(charset) with Strings in real world code! Might cause surprising results!
     println("${locale ?: "\t"} \t\t${string.toByteArray(charset).size} bytes \t\t $string")
 }
 
